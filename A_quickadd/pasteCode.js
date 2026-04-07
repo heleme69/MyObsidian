637ade79e13ad4fe70ce168b23f3366c9260386d
heleme69 <lehuyme9@gmail.com>
2026-04-07 20:24:31 +0700

    vault backup: 2026-04-07 20:24:31

--- A_quickadd/pasteCode.js
+++ A_quickadd/pasteCode.js
@@ -1,18 +1,29 @@
 module.exports = async (params) => {
-    const lang = await params.quickAddApi.inputPrompt("Language (matlab, python, js, ...)");
+    const useCodeBlock = await params.quickAddApi.suggester(
+        ["Plain text (no code block)", "Code block"],
+        [false, true]
+    );
+
     const levelStr = await params.quickAddApi.suggester(
-        ["None", "One level (> )", "Two levels (> >)"],
+        ["No prefix", "One level (> )", "Two levels (> >)"],
         ["", "> ", "> > "]
     );
-    
+
     const clip = await navigator.clipboard.readText();
     const b = String.fromCharCode(96);
     const fence = b+b+b;
     const prefix = levelStr;
-    let out = prefix + fence + lang + "\n";
-    clip.split("\n").forEach(l => out += prefix + l + "\n");
-    out += prefix + fence;
-    
+    let out = "";
+
+    if (useCodeBlock) {
+        const lang = await params.quickAddApi.inputPrompt("Language (matlab, python, js, ...)");
+        out += prefix + fence + lang + "\n";
+        clip.split("\n").forEach(l => out += prefix + l + "\n");
+        out += prefix + fence;
+    } else {
+        clip.split("\n").forEach(l => out += prefix + l + "\n");
+    }
+
     const view = app.workspace.getActiveViewOfType(params.obsidian.MarkdownView);
     const editor = view.editor;
     editor.replaceRange(out, editor.getCursor());
